5f7d9ace0eb6599e55919cfb95f7449a2229dc42
Ekischleki <e.wolfschlaeger@gmail.com>
2023-07-14 22:53:42 +0200

    Add project files.

---
+++ Keygen.cs
@@ -0,0 +1,212 @@
+﻿
+using System.Numerics;
+using DataTypeStoreLib;
+
+namespace RSAKeygenLib
+{
+    public class Keygen
+    {
+        public static PublicPrivateKeypair GenerateRSAKeypair(int lenght = 256)
+        {
+            
+            List<BigInteger> primes = GenPrimes.GenLargePrimes(2, Environment.ProcessorCount, lenght);
+            PublicPrivateKeypair result = GenKeypairWithPrimes(primes[0], primes[1]);
+            if (result.PrivateKey.CryptUsingKeypair(result.PublicKey.CryptUsingKeypair(1)) != 1) throw new Exception("Keypair wasn't generated successfully.");
+            return result;
+        }
+
+        private static PublicPrivateKeypair GenKeypairWithPrimes(BigInteger prime1, BigInteger prime2)
+        {
+            BigInteger n = prime1 * prime2;
+            BigInteger tOfN = (prime1 - 1) * (prime2 - 1);
+            BigInteger e = RandomBigInteger.GetRandom(2, tOfN - 1);
+            if (e < 0)
+                Console.WriteLine($"e should be between 2 and {tOfN - 1}, but it is\n{e}");
+            while (FindGCF(e, tOfN) != 1)
+            {
+                //e = ((e - 1) % (tOfN - 1)) + 2;
+                e++;
+                if (e > tOfN - 1)
+                {
+                    e = RandomBigInteger.GetRandom(2, tOfN - 1);
+                }
+            }
+            BigInteger d = FindEGCD(e, tOfN);
+            return new PublicPrivateKeypair(d, e, n);
+        }
+        private static BigInteger FindGCF(BigInteger a, BigInteger b)
+        {
+            BigInteger mod = 1;
+            while (mod != 0)
+            {
+                mod = a % b;
+                a = b;
+                b = mod;
+            }
+            return a;
+        }
+
+        private static BigInteger FindEGCD(BigInteger e, BigInteger tOfN) //Extended greatest common divisor algorithm:https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
+        {
+            //Currently kinda broken...
+            BigInteger o1 = tOfN;
+            BigInteger o2 = e;
+            BigInteger o3;
+            BigInteger t1 = tOfN;
+            BigInteger t2 = 1;
+            BigInteger t3;
+
+            while (o1 != 1)
+            {
+                o3 = o1 - (o2 * (o1 / o2));
+                t3 = t1 - (t2 * (o1 / o2));
+                if (o3 < 0)
+                    o3 = ProperBigIntModNegPos(o3, tOfN);
+                if (t3 < 0)
+                    t3 = ProperBigIntModNegPos(t3, tOfN);
+                o1 = o2;
+                t1 = t2;
+                o2 = o3;
+                t2 = t3;
+            }
+            return t1;
+        }
+        private static BigInteger ProperBigIntModNegPos(BigInteger a, BigInteger b) //For some weird reason is the mod calculation not handeled properly, neg % pos = pos, not neg
+        {
+            if (a.Sign != -1)
+            {
+                throw new Exception("The function ProperBigIntModNegPos can only be used, if a is negativ and b positiv");
+            }
+            if (b.Sign != 1)
+            {
+                throw new Exception("The function ProperBigIntModNegPos can only be used, if a is negativ and b positiv");
+            }
+            BigInteger tempMod = a * -1 % b;
+            if (tempMod == 0)
+                return 0;
+            return b - tempMod;
+
+        }
+    }
+
+
+
+    public class PublicPrivateKeypair
+    {
+        public KeyPair PrivateKey;
+        public KeyPair PublicKey;
+
+        public PublicPrivateKeypair(Region region)
+        {
+            List<Region> keyPairsRegions = region.FindSubregionWithNameArray("KeyPair").ToList();
+            List<KeyPair> keyPairs = new();
+            keyPairsRegions.ForEach(region => keyPairs.Add(new(region)));
+            if (keyPairs[0].KeyPairType == KeyPair.PUBLIC)
+            { // If first key is the public key, the second key must be private key, else turn it around.
+                PublicKey = keyPairs[0];
+                PrivateKey = keyPairs[1];
+            }
+            else
+            {
+                PublicKey = keyPairs[1];
+                PrivateKey = keyPairs[0];
+            }
+
+        }
+
+        public Region Save
+        {
+            get
+            {
+                Region region = new("PublicPrivateKeyPair", new List<Region>(), new());
+                region.SubRegions.Add(PrivateKey.Save);
+                region.SubRegions.Add(PublicKey.Save);
+                return region;
+            }
+        }
+        public PublicPrivateKeypair(BigInteger aPrivateKey, BigInteger aPublicKey, BigInteger aKeyPart2)
+        {
+            PrivateKey = new KeyPair(KeyPair.PRIVATE, aPrivateKey, aKeyPart2);
+            PublicKey = new KeyPair(KeyPair.PUBLIC, aPublicKey, aKeyPart2);
+        }
+        public void PrintKeypair()
+        {
+            Console.WriteLine($"Private Key:\n{PrivateKey.keyPartOne}\nPublic Key:\n{PublicKey.keyPartOne}\nKey part two:\n{PublicKey.keyPartTwo}");
+            return;
+        }
+    }
+
+    public class KeyPair
+    {
+        public BigInteger keyPartOne;
+        public BigInteger keyPartTwo;
+        public const string PUBLIC = "public";
+        public const string PRIVATE = "private";
+
+
+
+        public KeyPair(Region region)
+        {
+            keyPartOne = BigInteger.Parse(region.FindDirectValue("keyPartOne").value);
+            keyPartTwo = BigInteger.Parse(region.FindDirectValue("keyPartTwo").value);
+            switch (region.FindDirectValue("keyPairType").value)
+            {
+                case PUBLIC:
+                    keyPairType = PUBLIC;
+                    break;
+
+                case PRIVATE:
+                    keyPairType = PRIVATE;
+                    break;
+
+                default:
+                    throw new Exception("Invalid keyPairType in region");
+            }
+        }
+
+        public Region Save
+        {
+            get
+            {
+                Region region = new("KeyPair", new List<Region>(), new());
+                region.directValues.Add(new("keyPairType", keyPairType, false));
+                region.directValues.Add(new("keyPartOne", keyPartOne.ToString(), false));
+                region.directValues.Add(new("keyPartTwo", keyPartTwo.ToString(), false));
+                return region;
+            }
+        }
+
+
+        public KeyPair(string akeyPairType, BigInteger akeyPartOne, BigInteger akeyPartTwo)
+        {
+            keyPartOne = akeyPartOne;
+            keyPartTwo = akeyPartTwo;
+            keyPairType = ValidateKeyPairType(akeyPairType);
+        }
+        private string keyPairType;
+        public string KeyPairType
+        {
+            get { return keyPairType; }
+            set { keyPairType = ValidateKeyPairType(value); }
+        }
+
+        public BigInteger CryptUsingKeypair(BigInteger number)
+        {
+            return BigInteger.ModPow(number, keyPartOne, keyPartTwo);
+        }
+
+        private static string ValidateKeyPairType(string testType)
+        {
+            //Will return default value if invalid. Will return input value if valid
+            switch (testType.ToLower())
+            {
+                case PRIVATE:
+                    return PRIVATE;
+                case PUBLIC:
+                    return PUBLIC;
+                default:
+                    return PRIVATE;
+            }
+        }
+    }
+}
---
+++ Primegen.cs
@@ -0,0 +1,213 @@
+﻿using System.Numerics;
+using System.Security.Cryptography;
+
+namespace RSAKeygenLib
+{
+
+    public class GenPrimes
+    {
+        private static int allGeneratedPrimes = 0;
+
+        private static readonly int[] smallPrimes = new int[] //List of small primes for the PrimeQuickscan
+        {
+            2,
+            3,
+            5,
+            7,
+            11,
+            13,
+            17,
+            19,
+            23,
+            29
+        };
+        private static bool PrimeQuickscan(BigInteger number)
+        {
+            //Will check, if the number is devisable by a few small prime numbers. This will eliminate about half of the numbers tested and is way faster than the isPrime function.
+            foreach (int check in smallPrimes)
+            {
+                if (number % check == 0 && number != check)
+                    return false; //Is definetly not a prime
+            }
+            return true; //Could still be no prime
+        }
+        private static bool IsPrime(BigInteger number)
+        {
+            //Rabin miller algorithm for evaluating primes
+            if (number == 2)
+            {
+                return true;
+            }
+            if (number.IsEven)
+            {
+                Console.WriteLine("Number is even, so it can't be prime");
+                return false;
+            }
+            if (!PrimeQuickscan(number))
+                return false;
+            BigInteger s = number - 1;
+            BigInteger t = 0;
+            BigInteger v;
+            while (s % 2 == 0 && s != 0)
+            {
+                s /= 2;
+                t += 1;
+            }
+            for (long iteration = 0; iteration < 25; iteration++) // Would be cool if it could run in paralell
+            {
+                BigInteger a = RandomBigInteger.GetRandom(2, number - 1);
+                // generate a random number to check between 2 and number -1
+
+                v = BigInteger.ModPow(a, s, number);
+                if (v != 1)
+                {
+                    long i = 0;
+                    while (v != number - 1)
+                    {
+                        if (i == t - 1)
+                        {
+                            return false; // if we're in this part of the code, we're 100% shure, that this is not a prime
+                        }
+                        else
+                        {
+                            i += 1;
+                            v = BigInteger.ModPow(v, 2, number);
+                        }
+                    }
+                }
+            }
+            return true; // could still be, that this isn't a prime
+
+        }
+
+        private static readonly List<BigInteger> generatedLargePrimes = new ();
+        public static List<BigInteger> GenLargePrimes(int numOfPrimes, int threads, int bytelnght = 256)
+        {
+            //MULTITHREADING
+            generatedLargePrimes.Clear();
+            allGeneratedPrimes = 0;
+            Task[] tasks1 = new Task[threads];
+            
+            for (int i = 0; i < threads; i++)
+            {
+                tasks1[i] = Task.Run(() => FindLargePrimes(bytelnght, numOfPrimes, "toList")); // this will ensure, that all primes will be generated
+            }
+
+            Task.WaitAny(tasks1);
+            
+            return generatedLargePrimes;
+        }
+
+        private static readonly object lockAllGeneratedPrimes = new();
+        private static void FindLargePrimes(int byteLenght, int amtOfLargePrimes, string outputPath)
+        {
+            //Console.WriteLine($"Started thread with byteLenght: {byteLenght}, amtOfLargePrimes: {amtOfLargePrimes}, outputPath: {outputPath}");
+            BigInteger randomLargeNum;
+            try
+            
+            {
+                while (allGeneratedPrimes < amtOfLargePrimes)
+                {
+
+                    do
+                    {
+                        randomLargeNum = GetRandomByteLenghtBigIntiger(byteLenght);
+                        if (randomLargeNum.IsEven)
+                            randomLargeNum++; // even nubers will not be tested, because they're (almost) always no primes (exept 2)
+                        if (allGeneratedPrimes >= amtOfLargePrimes)
+                            return;
+
+                    } while (!IsPrime(randomLargeNum));
+                    lock (lockAllGeneratedPrimes)
+                    {
+                        if (allGeneratedPrimes >= amtOfLargePrimes)
+                            return;
+                        allGeneratedPrimes++;
+                        if ("toList" == outputPath)
+                            generatedLargePrimes.Add(randomLargeNum);
+                        else
+                        {
+                            File.AppendAllText(outputPath, Convert.ToString(randomLargeNum) + "\n\n");
+                        }
+                    }
+                }
+                return;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error happened!?");
+                Console.WriteLine(ex);
+                Console.ReadKey();
+            }
+        }
+
+
+
+
+
+
+
+
+
+        private static readonly RandomNumberGenerator rng = RandomNumberGenerator.Create();
+        internal static BigInteger GetRandomLenghtBigIntiger(int lenght)
+        {
+            byte[] data = new byte[lenght];
+            List<int> dataInt = new();
+            rng.GetBytes(data);
+            foreach (byte Byte in data)
+                dataInt.Add(Byte);
+            int[] randomLenght = new int[lenght];
+
+            for (int i = 0; i < lenght; i++)
+            {
+                if (i == 0)
+                    randomLenght[i] = (dataInt[i] % 9) + 1;
+                else
+                    randomLenght[i] = dataInt[i] % 10;
+
+
+            }
+
+            return BigInteger.Parse(ConvertArrayToString.ConvertIntArrayToString(randomLenght)); //Perfectly clean lol
+        }
+        internal static BigInteger GetRandomByteLenghtBigIntiger(int byteLenght)
+        {
+            byte[] resultBytes = new byte[byteLenght];
+            rng.GetBytes(resultBytes);
+            BigInteger result = new(resultBytes);
+            if (result.Sign == -1)
+                result *= -1;
+            return result;
+        }
+    }
+    internal class ConvertArrayToString
+    {
+        internal static string ConvertIntArrayToString(int[] array)
+        {
+            string resolution = "";
+
+            foreach (int item in array)
+            {
+                resolution += Convert.ToString(item);
+            }
+            return resolution;
+        }
+
+    }
+
+    internal class RandomBigInteger
+    {
+        internal static BigInteger GetRandom(BigInteger min, BigInteger max)
+        {
+            max++;
+            int lenght = (Convert.ToString(max) ?? throw new Exception("Convert.ToString(max) returned null.")).Length - (Convert.ToString(min) ?? throw new Exception("Convert.ToString(min) returned null.")).Length + 10;
+            BigInteger result = RSAKeygenLib.GenPrimes.GetRandomLenghtBigIntiger(lenght);
+            return (result % (max - min)) + min;
+
+        }
+
+
+    }
+
+}
